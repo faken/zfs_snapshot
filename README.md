4126e21484a49238acd27d36bde7b4174b873341
Sascha Held <saschaheld@me.com>
2017-06-04 13:19:20 +0200

    initial comit

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # zfs_snapshot
 Ruby script for creating periodic ZFS snapshots (incl. cleanup feature)
+
+Dependencies: 
+- time_difference gem
+
+Usage: 
+Recommended to be used in combination with cron jobs to automate the cleanup / creation of snapshots. 
+Needs root privileges for creating / destroying snapshots.
+
+```
+Usage: zfs_snapshot [options]
+    -d, --dataset DATASET            ZFS Dataset to work on
+    -t, --ttl TIME IN DAYS           Amount of time the snapshot should be valid, supported values hours(h), days(d), months(m), years(y)
+    -n, --name SNAPSHOT NAME         Basic name of the snapshot
+    -v, --verbose                    Output more information
+    -i, --info                       Shows statistics about created snapshots
+    -r, --recursive                  Create / Destroy ZFS Snapshots recursively
+    -s, --safe                       Safe mode, dont execute actual command
+    -c, --create                     Creates a new snapshot with the given values
+    -p, --purge                      Deletes expired ZFS Snapshots
+```
+
+Examples: 
+
+Create new snapshot of tank-dataset that's valid for 30 days
+``zfs_snapshot -c -d tank -t 30d``
+
+Create new snapshot of tank-dataset that's valid for 30 weeks
+``zfs_snapshot -c -d tank -t 30w``
+
+Check for expired snapshots and automatically delete them
+``zfs_snapshot -d tank -p`
+
+#Generate a new snapshot every hour thats valid for 12 hours
+0 */2 * * * sudo ruby zfs_snapshot.rb -c -d tank/jails/services/jira -t 12h >/dev/null 2>&1 
+
+#Generate a new snapshot every day at midnight that's valid for 30 days
+0 0 * * * sudo ruby zfs_snapshot.rb -c -d tank/jails/services/jira -t 30d >/dev/null 2>&1
+
+#Generate a new snapshot at the beginning of a month at midnight that's valid for 3 months
+0 0 1 * * sudo ruby zfs_snapshot.rb -c -d tank/jails/services/jira -t 3m >/dev/null 2>&1
+
+#Look for outdated snapshots every day at midnight and automatically delete them
+0 0 * * * sudo ruby zfs_snapshot.rb -d tank/jails/services/jira -p >/dev/null 2>&1
---
+++ zfs_snapshot.rb
@@ -0,0 +1,149 @@
+require 'optparse'
+require 'open3'
+require 'date'
+require 'time_difference'
+
+options = {}
+
+OptionParser.new do |parser|
+  options[:dataset] = nil
+  parser.on( '-d', '--dataset DATASET', 'ZFS Dataset to work on' ) do|dataset|
+    options[:dataset] = dataset
+  end
+
+  options[:ttl] = nil
+  parser.on( '-t', '--ttl TIME IN DAYS', 'Amount of DAYS the created backup should be kept') do|ttl|
+    options[:ttl] = ttl
+  end
+
+  options[:snapshot_name] = nil
+  parser.on( '-n', '--name SNAPSHOT NAME', 'Basic name of the snapshot') do |name|
+    options[:snapshot_name] = name
+  end
+
+  options[:verbose] = false
+  parser.on( '-v', '--verbose', 'Output more information' ) do
+    options[:verbose] = true
+  end
+
+  parser.on('-i', '--info', 'Shows statistics about created snapshots') do
+    options[:stats] = true
+  end
+
+  parser.on('-r', '--recursive', 'Create / Destroy ZFS Snapshots recursively') do
+    options[:recursive] = true
+  end
+
+  parser.on('-s', '--safe', 'Safe mode, dont execute actual command') do
+    options[:safe_mode] = true
+  end
+
+  parser.on('-c', '--clean', 'Deletes expired ZFS Snapshots') do
+    options[:clean] = true
+  end
+end.parse!
+
+def create_zfs_snapshot(dataset, ttl, snapshot_name, safe_mode=false, recursive=false, verbose=false)
+  date = Time.now.utc.strftime('%Y%m%d-%H%M')
+  command = "zfs snapshot #{recursive ? '-r' : nil} #{dataset}@#{snapshot_name}_#{date}_exp_#{ttl}"
+  execute_system_command(command,safe_mode, verbose)
+end
+
+def destroy_outdated_snapshots(dataset, recursive=false, safe_mode=false, verbose=false)
+  puts 'Looking for outdated snapshots...'
+
+  output = get_snapshots(dataset, verbose)
+  output.split(/\r?\n|\r/).each do |line|
+    snapshot_age = snapshot_age(line)
+
+    if snapshot_age[0] > snapshot_age[1]
+      puts "Destroying snapshot: #{line}"
+      command = "zfs destroy #{recursive ? '-r' : nil} #{line}"
+      execute_system_command(command, safe_mode)
+    end
+  end
+end
+
+def show_statistics(dataset, verbose)
+  output = get_snapshots(dataset, verbose)
+  output.split(/\r?\n|\r/).each do |line|
+    created_at_match = line.match('[0-9]{8}-[0-9]{4}')
+
+    if created_at_match
+      created_at = DateTime.strptime(created_at_match[0], '%Y%m%d-%H%M')
+      age = snapshot_age(line)
+
+      if created_at
+        puts '--------------------------------------------------------'
+        puts "Snapshot: #{line}"
+        puts "Created: #{created_at}"
+        puts "Age: #{age[0]}"
+        puts "Expired: #{age[0] > age[1]}"
+      end
+    end
+  end
+  puts '--------------------------------------------------------'
+end
+
+def get_snapshots(dataset, verbose)
+  execute_system_command("zfs list -H -o name -t snapshot | sort | grep #{dataset}", false, verbose)
+end
+
+def snapshot_age(snapshot_name)
+  exp_match = snapshot_name.match('exp_(([0-9]*)([hdmy]))')
+  created_at_match = snapshot_name.match('[0-9]{8}-[0-9]{4}')
+
+  if exp_match && created_at_match
+    exp = exp_match[2]
+    exp_unit = exp_match[3]
+    created_at = DateTime.strptime(created_at_match[0], '%Y%m%d-%H%M')
+
+    if exp && exp_unit && created_at
+      age_unit_selector_mapping = {h: 'in_hours', d: 'in_days', m: 'in_months', y: 'in_years'}
+      age_unit_selector = age_unit_selector_mapping[exp_unit.downcase.intern]
+
+      if age_unit_selector
+        return [TimeDifference.between(DateTime.now.utc , created_at.utc).send(age_unit_selector), exp.to_f, exp_unit]
+      end
+    end
+  end
+end
+
+def execute_system_command(cmd, safe_mode=false, verbose=false)
+  if safe_mode || verbose
+    puts "Executing command: #{cmd}"
+  end
+
+  unless safe_mode
+    Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
+      exit_status = wait_thr.value
+      unless exit_status.success?
+        abort "Failure executing command: #{cmd}"
+      end
+
+      return stdout.read
+    end
+  end
+end
+
+raise OptionParser::MissingArgument if options[:dataset].nil?
+
+if options[:stats]
+  show_statistics(options[:dataset], options[:verbose])
+  abort
+end
+
+raise OptionParser::MissingArgument if options[:snapshot_name].nil?
+raise OptionParser::MissingArgument if options[:ttl].nil?
+
+create_zfs_snapshot(options[:dataset], options[:ttl],
+                    options[:snapshot_name], options[:safe_mode],
+                    options[:recursive],options[:verbose])
+
+if options[:clean]
+  destroy_outdated_snapshots(options[:dataset],
+                             options[:recursive],
+                             options[:safe_mode],
+                             options[:verbose])
+end
+
